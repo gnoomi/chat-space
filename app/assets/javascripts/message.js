80c0b67f0c01228a63da1afe46fdd84e5533955c
gnoomi <masaomiomi0042@gmail.com>
2019-11-25 19:14:17 +0900

    コンソール消した

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -46,7 +46,6 @@ $(function(){
     var reloadMessages = function() {
       if(window.location.href.match(/\/groups\/\d+\/messages/)){  
         var last_message_id = $(".messages .message:last").data("message-id");
-        console.log(last_message_id)
         $.ajax({
           url: "api/messages",
           type: 'GET',
